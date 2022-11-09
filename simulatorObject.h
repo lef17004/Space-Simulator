f09549b33b45b7e221944198f3c4ccb9523fbb37
Michael LeFevre <64995133+lef17004@users.noreply.github.com>
2022-11-09 11:36:44 -0700

    Stuff

--- simulatorObject.h
+++ simulatorObject.h
@@ -6,3 +6,20 @@
 //
 
 #pragma once
+#include "position.h"
+#include "velocity.h"
+#include "collisionBody.h"
+#include "wreckage.h"
+
+class SimulatorObject
+{
+private:
+   Position position;
+   Velocity velocity;
+   int radius;
+   bool isAlive;
+   
+public:
+   SimulatorObject() {}
+   
+};
