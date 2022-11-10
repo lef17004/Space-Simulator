a608b1ae31cb8dbaebf03ee534b56369218ee76c
Michael LeFevre <64995133+lef17004@users.noreply.github.com>
2022-11-10 14:08:57 -0700

    stubs

--- testSimulatorObject.h
+++ testSimulatorObject.h
@@ -12,6 +12,151 @@ class TestSimulatorObject
 public:
    void run()
    {
+      test_defaultConstructor();
+      test_nonDefaultConstructor();
+      test_advanceFrameOne();
+      test_advanceFrameTwo();
+      test_advanceFrameThree();
+      test_hitNoWreckage();
+      test_hitWreckage();
+      test_rotate();
+      test_getCollisionBody();
+      test_getRadius();
+      test_setRadius();
+      test_getPosition();
+      test_setPosition();
+      test_getX();
+      test_getY();
+      test_setX();
+      test_setY();
+      test_getVelocity();
+      test_setVelocity();
+      test_getDx();
+      test_setDx();
+      test_getDy();
+      test_setDy();
       
    }
+   
+   void test_defaultConstructor()
+   {
+      
+   }
+   
+   void test_nonDefaultConstructor()
+   {
+      
+   }
+   
+   void test_advanceFrameOne()
+   {
+      
+   }
+   
+   void test_advanceFrameTwo()
+   {
+      
+   }
+   
+   void test_advanceFrameThree()
+   {
+      
+   }
+   
+   void test_hitNoWreckage()
+   {
+      
+   }
+   
+   void test_hitWreckage()
+   {
+      
+   }
+   
+   void test_rotate()
+   {
+      
+   }
+   
+   void test_getCollisionBody()
+   {
+      
+   }
+   
+   void test_getRadius()
+   {
+      
+   }
+   
+   void test_setRadius()
+   {
+      
+   }
+   
+   void test_getPosition()
+   {
+      
+   }
+   
+   void test_setPosition()
+   {
+      
+   }
+   
+   void test_getX()
+   {
+      
+   }
+   
+   void test_setX()
+   {
+      
+   }
+   
+   void test_getY()
+   {
+      
+   }
+   
+   void test_setY()
+   {
+      
+   }
+   
+   void test_getVelocity()
+   {
+      
+   }
+   
+   void test_setVelocity()
+   {
+      
+   }
+   
+   void test_getDx()
+   {
+      
+   }
+   
+   void test_setDx()
+   {
+      
+   }
+   
+   void test_getDy()
+   {
+      
+   }
+   
+   void test_setDy()
+   {
+      
+   }
+   
+   void test_location()
+   {
+      
+   }
+   
 };
+
