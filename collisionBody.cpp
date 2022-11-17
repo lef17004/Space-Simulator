638945c45186ec6bbf5a253048b00597ae788f40
Jane Orr <djaneorr@gmail.com>
2022-11-17 13:33:45 -0700

    edited collisionBody.cpp

--- collisionBody.cpp
+++ collisionBody.cpp
@@ -6,3 +6,22 @@
 //
 
 #include "collisionBody.h"
+#include <algorithm>
+#include <minwindef.h>
+
+static bool isCollision(CollisionBody box1, CollisionBody box2) {
+  
+   using std::abs;
+
+   float dMax = max(abs(box1.getDx()), abs(box1.getDy()));
+   float dMax = max(dMax, abs(box2.getDx()));
+   float dMax = max(dMax, abs(box2.getDy()));
+
+   float distMin = std::numeric_limits<float>::max();
+
+   float slice = 1.0 / dMax;
+
+   
+
+   
+}
