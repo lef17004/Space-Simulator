dec6a3efd1b8cea481aa1ff9d87fe3b9c13dac87
Jane Orr <djaneorr@gmail.com>
2022-11-17 15:03:04 -0700

    Comments and comment blocks for collision body

--- collisionBody.cpp
+++ collisionBody.cpp
@@ -11,10 +11,12 @@
 using namespace std;
 
 /******************************************************************************
- *
+ * This class gets the points of the various bodies and will get the distances of 
+ * the bodies to see if they collide.
  ******************************************************************************/
 bool CollisionBody::isCollision(CollisionBody box1, CollisionBody box2) {
   
+   // Finding the maximum distance travelled
    using std::abs;
 
    double dMax = max(abs(box1.getDx()), abs(box1.getDy()));
@@ -22,16 +24,21 @@ bool CollisionBody::isCollision(CollisionBody box1, CollisionBody box2) {
    dMax = max(dMax, abs(box2.getDy()));
    dMax = max(dMax, 0.1);
 
+   // Advance number
    float  distMin = std::numeric_limits<double>::max();
 
+   // Percent of the speed
    double slice = 1.0 / dMax;
 
+   // Cycling through percentage points
    for (float percent = 0.0; percent <= 1.0; percent += slice) {
+      // Finding the points of the LHS and the RHS manually
       float x1 = box1.getX() + (box1.getDx() * percent);
       float y1 = box1.getY() + (box1.getDy() * percent);
       float x2 = box2.getX() + (box2.getDx() * percent);
       float y2 = box2.getY() + (box2.getDy() * percent);
       
+      // How far apart they are now
       float distSquared = ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
 
       distMin = min(distMin, distSquared);
