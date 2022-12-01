7a09d04161eace3cca1b0559d0716c00b4b7aaf3
Michael LeFevre <64995133+lef17004@users.noreply.github.com>
2022-12-01 15:47:53 -0700

    Life Span Complete

--- lifespan.h
+++ lifespan.h
@@ -6,3 +6,17 @@
 //
 
 #pragma once
+class LifeSpan
+{
+private:
+   int life;
+public:
+   LifeSpan(): life(70) {};
+   LifeSpan(int life): life(life)
+   {
+   }
+   
+   void advance() { life--; }
+   bool isAlive() const { return life > 0; }
+   void setLife(int life) { this->life = life; }
+};
